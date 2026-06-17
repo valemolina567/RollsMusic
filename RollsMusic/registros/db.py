f4fd84819b0e4e9b38daa1da0fa94455ddb96b39
Mateo Enríquez <mateo.enriquez@udla.edu.ec>
2026-06-17 16:20:59 -0500

    Conexión a MongoDB Atlas y seguridad implementada

--- RollsMusic/registros/db.py
+++ RollsMusic/registros/db.py
@@ -1,7 +1,19 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-# Conexión a tu servidor local de MongoDB (asegúrate de tener MongoDB Compass abierto y corriendo)
-client = MongoClient('mongodb://localhost:27017/')
+# 1. Cargamos las variables ocultas del archivo .env
+load_dotenv()
 
-# Seleccionamos la base de datos de tu proyecto
-db = client['RollsMusicDB']
+# 2. Obtenemos la URI de forma segura
+URI_ATLAS = os.getenv("MONGO_URI")
+
+# 3. Conectamos a Atlas
+try:
+    client = MongoClient(URI_ATLAS)
+    db = client['RollsMusicDB'] 
+    # Opcional: Esto hace un ping rápido para verificar que la conexión es exitosa
+    client.admin.command('ping')
+    print("Conexión a MongoDB Atlas exitosa.")
+except Exception as e:
+    print(f"Error conectando a MongoDB Atlas: {e}")
